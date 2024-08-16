96f7d8b786af043faf9261754215ec6a7cc76ccf
esenbekovnurgazy <esenbekovnurgazy.2008.kg@gmail.com>
2024-08-16 21:15:12 +0600

    design continue

--- lib/features/search/presentation/screen/city_search_page.dart
+++ lib/features/search/presentation/screen/city_search_page.dart
@@ -9,7 +9,7 @@ class SearchWeatherPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: Colors.blueAccent,
+      backgroundColor: Colors.lightBlueAccent,
       body: Center(
         child: _Body(),
       ),
@@ -34,10 +34,30 @@ class _Body extends StatelessWidget {
               padding: const EdgeInsets.all(16.0),
               child: _SearchTextField(),
             ),
-              _SearchWeatherIcon(),
-            _SearchIcons(),
+            Column(
+              children: [
+                _LocationCityNameText(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _TempText(),
+                    _SearchWeatherIcon(),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _TempIcon(),
+                    _HumidityIcon(),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [_PressureWidget()],
+                ),
+              ],
+            ),
             _SearchText(),
-            // _SearchText2(),
           ],
         );
       },
@@ -45,8 +65,170 @@ class _Body extends StatelessWidget {
   }
 }
 
-class _SearchIcons extends StatelessWidget {
-  const _SearchIcons({super.key});
+class _PressureWidget extends StatelessWidget {
+  const _PressureWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          width: 150,
+          height: 100,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  Icons.compress,
+                  size: 50,
+                  color: Colors.yellow,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    BlocBuilder<CitySearchBloc, CitySearchState>(
+                      builder: (context, state) {
+                        final notNullVariable = state.data?.current?.pressureMb ?? 0;
+                        final pressure = notNullVariable * 0.75;
+                        return Text(
+                          '${pressure.toInt()}',
+                          style: TextStyle(fontSize: 30),
+                        );
+                      },
+                    ),
+                    Text(
+                      'мб',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    // SvgPicture.asset(
+                    //   'assets/icons/ic_snow.svg',
+                    //   width: 70,
+                    // ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(40)),
+        ),
+      ],
+    );
+  }
+}
+
+class _LocationCityNameText extends StatelessWidget {
+  const _LocationCityNameText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CitySearchBloc, CitySearchState>(
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.only(left: 50.0),
+          child: Row(
+            children: [
+              Text(
+                '${state.data?.location?.name}',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _TempText extends StatelessWidget {
+  const _TempText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CitySearchBloc, CitySearchState>(
+      builder: (context, state) {
+        return Row(
+          children: [
+            Text(
+              '${state.data?.current?.tempC}',
+              style: TextStyle(color: Colors.white, fontSize: 70),
+            ),
+            Text(
+              '°',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 70,
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _TempIcon extends StatelessWidget {
+  const _TempIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          width: 150,
+          height: 100,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  Icons.pan_tool,
+                  size: 50,
+                  color: Colors.yellow,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    BlocBuilder<CitySearchBloc, CitySearchState>(
+                      builder: (context, state) {
+                        return Text(
+                          '${state.data?.current?.feelslikeC}',
+                          style: TextStyle(fontSize: 30),
+                        );
+                      },
+                    ),
+                    Text(
+                      '°C',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    // SvgPicture.asset(
+                    //   'assets/icons/ic_snow.svg',
+                    //   width: 70,
+                    // ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(40)),
+        ),
+      ],
+    );
+  }
+}
+
+class _HumidityIcon extends StatelessWidget {
+  const _HumidityIcon({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -57,27 +239,37 @@ class _SearchIcons extends StatelessWidget {
           width: 150,
           height: 100,
           child: Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                BlocBuilder<CitySearchBloc, CitySearchState>(
-                  builder: (context, state) {
-                    return Text(
-                      '${state.data?.current?.tempC}',
-                      style: TextStyle(fontSize: 30),
-                    );
-                  },
+                Icon(
+                  Icons.opacity,
+                  color: Colors.blue,
+                  size: 50,
                 ),
-                Text(
-                  '°C',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    BlocBuilder<CitySearchBloc, CitySearchState>(
+                      builder: (context, state) {
+                        return Text(
+                          '${state.data?.current?.humidity}',
+                          style: TextStyle(fontSize: 30),
+                        );
+                      },
+                    ),
+                    Text(
+                      '%',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    // SvgPicture.asset(
+                    //   'assets/icons/ic_snow.svg',
+                    //   width: 70,
+                    // ),
+                  ],
                 ),
-                // SvgPicture.asset(
-                //   'assets/icons/ic_snow.svg',
-                //   width: 70,
-                // ),
               ],
             ),
           ),
@@ -168,4 +360,3 @@ class _HumidityWidget extends StatelessWidget {
     return const Placeholder();
   }
 }
-
